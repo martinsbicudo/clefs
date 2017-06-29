f47f1b010358d158b6cd71d76953dc0393360c16
martinsbicudo <victor_bicudo@outlook.com>
2017-06-29 16:38:55 -0300

    Last updates

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+//REQUIRES
+const gulp = require('gulp')
+
+  //POSTCSS
+  , postcss = require('gulp-postcss')
+  , cssnano = require('cssnano')
+  , cssnext = require('postcss-cssnext')
+
+  //DEFAULT
+  , sass = require('gulp-sass')
+  , sourcemaps = require('gulp-sourcemaps')
+  , ext_replace = require('gulp-ext-replace');
+
+//PATHS FILES
+const paths = {
+	src : {
+		css 	: {
+		  sass : 'assets/clefs/clefs.scss',
+		  watch : 'assets/clefs/**/*.scss',
+		}
+	} ,
+	dest : {
+		css 	: 	'css/'
+	}
+}
+
+//TASKS
+gulp.task('css', function(){
+  gulp.src( paths.src.css.sass )
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      cssnext()
+    ]).on('error', function(err) {
+        console.log(err.message);
+    }))
+    .pipe(gulp.dest( paths.dest.css ))
+    .pipe(postcss([
+      cssnano
+    ]))
+    .pipe(ext_replace('.min.css'))
+    .pipe(sourcemaps.write( '.' ))
+    .pipe(gulp.dest( paths.dest.css ));
+});
+
+//WATCH
+gulp.task('watch', function(){
+  gulp.watch(paths.src.css.watch, ['css']);
+});
+
+gulp.task('default', ['css', 'watch']);
